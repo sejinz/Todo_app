de6dccc1fb7e166c8094eef74cc0e66fce6621e8
sejinz <zzinnzz1@gmail.com>
2022-04-20 10:12:42 +0900

    TODO_LIST

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,27 +1,27 @@
 import React,{useState,useCallback} from 'react';
 import { MdAdd } from "react-icons/md";
-import '../styles/TodoInsert.scss'
+import '../styles/TodoInsert.scss';
 
 function TodoInsert({onInsert}) {
-  const [value, setValue] = useState(' ');
+  const [value, setValue] = useState('');
 
   const onChange = useCallback(e => {
-    setValue(e.target.value);
+      setValue(e.target.value);
   },[]);
 
   const onSubmit = useCallback(e => {
-    onInsert(value);
-        setValue(' ');
-        e.preventDefault();
-        //submit 이벤트는 브라우저에서 새로고침을 발생시킨다.
+      onInsert(value);
+      setValue('');  
+      e.preventDefault();
+      //submit이벤트는 브라우저에서 새로고침을 발생시킨다
   },[value]);
 
   return (
     <form className='TodoInsert' onSubmit={onSubmit}>
-      <input placeholder='오늘 해야할 일 !! '
-      value ={value}
-      onChange={onChange} />
-      <button type='submit'><MdAdd /></button>
+        <input placeholder='할 일을 입력하세요'
+            value={value} 
+            onChange={onChange} />
+        <button type='submit'><MdAdd /></button>
     </form>
   )
 }
